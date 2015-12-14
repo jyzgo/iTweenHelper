267d01662217b70b111d58b29c5733febefa7dff
jyzgo <jiangyizheng@magictavern.com>
2015-12-14 11:08:42 +0800

    initial commit

---
+++ Assets/exampleCurve.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exampleCurve : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		curEvent = GetComponent<iTweenEvent>();
+	}
+	iTweenEvent curEvent;
+
+	public Vector3 TargetPos;
+
+	void OnGUI()
+	{
+		if(GUILayout.Button("Play"))
+		{
+			curEvent.Play();
+		}
+	}
+
+}
